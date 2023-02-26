0c5c26ec66b56d6d08fa643646e3fb1a196c879f
游齐宇 <55417548+Ysofun@users.noreply.github.com>
2023-02-26 11:27:55 +0800

    2023/2/26 新题目

--- LeetCode/m0811.h
+++ LeetCode/m0811.h
@@ -2,6 +2,17 @@
 class Solution {
 public:
     int waysToChange(int n) {
+        vector<int> coins = { 1, 5, 10, 25 };
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (j >= coins[i]) { dp[j] = (dp[j] + dp[j - coins[i]]) % 1000000007; }
+            }
+        }
 
+        return dp[n];
     }
 };
--- LeetCode/m0813.h
+++ LeetCode/m0813.h
@@ -1,16 +1,28 @@
 #pragma once
 class Solution {
 public:
-
-
     int pileBox(vector<vector<int>>& box) {
         auto cmp = [](const vector<int>& x, const vector<int>& y)
         {
-            return x[2] < y[2];
+            return x[2] > y[2];
         };
 
         sort(box.begin(), box.end(), cmp);
 
-
+        int n = box.size(), ans = 0;
+        vector<int> dp(n);
+        for (int i = 0; i < n; i++)
+        {
+            dp[i] = box[i][2];
+            for (int j = 0; j < i; j++)
+            {
+                if (box[i][0] < box[j][0] && box[i][1] < box[j][1] && box[i][2] < box[j][2])
+                {
+                    dp[i] = max(dp[i], box[i][2] + dp[j]);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
     }
 };
